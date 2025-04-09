c4e4186f847d881526b54d19ab5c778b7ddcd0e4
Surya <125463872+Surya-sourav@users.noreply.github.com>
2025-04-09 16:19:32 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,14 @@ import messageRoutes from './routes/messageRoutes';
 const app = express();
 
 // Middleware
-app.use(cors(
-  {
-      origin : 'https://campaign-management-frontend.vercel.app/ , https://campaign-management-frontend-git-main-surya-souravs-projects.vercel.app/ , https://campaign-management-frontend-dv86b4er0-surya-souravs-projects.vercel.app/'
-  }
-));
+app.use(cors({
+  origin: [
+    'https://campaign-management-frontend.vercel.app',
+    'https://campaign-management-frontend-git-main-surya-souravs-projects.vercel.app',
+    'https://campaign-management-frontend-dv86b4er0-surya-souravs-projects.vercel.app'
+  ]
+}));
+
 app.use(express.json());
 
 // Routes
